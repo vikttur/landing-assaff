f5a3ec78b1d2b3eda2bb28c293faa2fdcd16a5ae
vikttur <105513387+vikttur@users.noreply.github.com>
2023-11-16 12:14:55 +0000

    Deploying to gh-pages from @ vikttur/landing-assaff@541da359471dcd7c0d68719991ef70beedbdf802 🚀

---
+++ index.c273d38c.js
@@ -0,0 +1,3 @@
+!function(){var o=this,n=$(".back-to-top");$(window).on("scroll",function(){$(o).scrollTop()>=80?n.fadeIn():n.fadeOut()}),n.on("click",function(o){o.preventDefault(),$("html").animate({scrollTop:0},1e3)})}();//# sourceMappingURL=index.c273d38c.js.map
+
+//# sourceMappingURL=index.c273d38c.js.map
