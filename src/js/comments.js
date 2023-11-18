e5e4b57e26021f855a5b98053fa1480869348797
Viktor Biruk <vikttur.job@gmail.com>
2023-11-18 18:31:16 +0200

    creates a photo gallery

---
+++ src/js/comments.js
@@ -0,0 +1,31 @@
+new Swiper('.text-slider', {
+	loop: true,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+	},
+	autoplay: {
+		delay: 3500,
+		stopOnLastSlide: true,
+		disableOnInteraction: false,
+	},
+	grabCursor: true,
+	slideToClickedSlide: true,
+	speed: 2000,
+	// effect: 'fade',
+	// effect: 'flip',
+	// flipEffect: {
+	// 	slideShadows: true,
+	// 	limitRotation: true,
+	// },
+	effect: 'coverFlow',
+	coverFlowEffect: {
+		rorate: 20,
+		stretch: 50,
+		slideShadows: true,
+	},
+	centeredSlides: true,
+	autoHeight: true,
+	// slidePerView: 'auto',
+	// watchOverflow: true,
+});
---
+++ src/js/gallery.js
@@ -0,0 +1,84 @@
+new Swiper('.image-slider', {
+	loop: true,
+	// navigation: {
+	// 	prevEl: 'swiper-button-prev',
+	// 	nextEl: 'swiper-button-next',
+	// },
+	  pagination: {
+    el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+  },
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// 	draggable: true,
+	// },
+	// hashNavigation: {
+	// 	watchState: true,
+	// },
+	autoplay: {
+		delay: 2500,
+		stopOnLastSlide: true,
+		disableOnInteraction: false,
+	},
+	// preloadImages: false,
+	// lazy: {
+	// 	loadOnTransitionStart: false,
+	// 	loadPrevNext:false,
+	// },
+	// watchSlidesProgress: true,
+	// watchSlidesVisability: true,
+
+	// keyboard: {
+	// 	enabled: true,
+	// 	onlyInVievport: true,
+	// 	pageUpDown: true,
+	// },
+	grabCursor: true,
+	slideToClickedSlide: true,
+	speed: 1500,
+	// effect: 'fade',
+	// effect: 'flip',
+	// flipEffect: {
+	// 	slideShadows: true,
+	// 	limitRotation: true,
+	// },
+	effect: 'cube',
+	cubeEffect: {
+		slideShadows: true,
+		shadow: true,
+		shadowOffset: 20,
+		shadowScale:0.9,
+	},
+	// zoom: {
+	// 	maxRatio:5,
+	// 	minRatio: 1,
+	// },
+	// effect: 'coverFlow',
+	// coverFlowEffect: {
+	// 	rorate: 20,
+	// 	stretch: 50,
+	// 	slideShadows: true,
+	// },
+	// slidePerColumn: 2,
+	// centeredSlides: true,
+	// autoHeight: true,
+	// slidePerView: 'auto',
+	// watchOverflow: true,
+
+	// mousewheel: {
+	// 	sensitiviti: 1,
+	// 	eventsTarget: ".image-slider",
+	// },
+	// breakpoints: {
+	// 	320: {
+	// 		slidePerView: 1,
+	// 	},
+	// 	480: {
+	// 		slidePerView: 2,
+	// 	},
+	// 	768: {
+	// 		slidePerView: 3,
+	// 	},
+	// },
+});
