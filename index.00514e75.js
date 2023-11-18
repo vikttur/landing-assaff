4e44ff4fb502aa678247ec52a2f0812cd13b46ff
vikttur <105513387+vikttur@users.noreply.github.com>
2023-11-18 16:31:48 +0000

    Deploying to gh-pages from @ vikttur/landing-assaff@e5e4b57e26021f855a5b98053fa1480869348797 🚀

---
+++ index.00514e75.js
@@ -0,0 +1,32 @@
+new Swiper(".image-slider",{loop:!0,// navigation: {
+// 	prevEl: 'swiper-button-prev',
+// 	nextEl: 'swiper-button-next',
+// },
+pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},// scrollbar: {
+// 	el: '.swiper-scrollbar',
+// 	draggable: true,
+// },
+// hashNavigation: {
+// 	watchState: true,
+// },
+autoplay:{delay:2500,stopOnLastSlide:!0,disableOnInteraction:!1},// preloadImages: false,
+// lazy: {
+// 	loadOnTransitionStart: false,
+// 	loadPrevNext:false,
+// },
+// watchSlidesProgress: true,
+// watchSlidesVisability: true,
+// keyboard: {
+// 	enabled: true,
+// 	onlyInVievport: true,
+// 	pageUpDown: true,
+// },
+grabCursor:!0,slideToClickedSlide:!0,speed:1500,// effect: 'fade',
+// effect: 'flip',
+// flipEffect: {
+// 	slideShadows: true,
+// 	limitRotation: true,
+// },
+effect:"cube",cubeEffect:{slideShadows:!0,shadow:!0,shadowOffset:20,shadowScale:.9}});//# sourceMappingURL=index.00514e75.js.map
+
+//# sourceMappingURL=index.00514e75.js.map
