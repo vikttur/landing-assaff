7629ba326f981680d460aeb69d33d62d85859369
vikttur <105513387+vikttur@users.noreply.github.com>
2023-11-18 17:16:16 +0000

    Deploying to gh-pages from @ vikttur/landing-assaff@a39966ae1716472e7c3f7d8869280df7587a088d 🚀

--- index.928ed886.js
+++
@@ -1,9 +0,0 @@
-new Swiper(".text-slider",{loop:!0,scrollbar:{el:".swiper-scrollbar",draggable:!0},autoplay:{delay:3500,stopOnLastSlide:!0,disableOnInteraction:!1},grabCursor:!0,slideToClickedSlide:!0,speed:2e3,// effect: 'fade',
-// effect: 'flip',
-// flipEffect: {
-// 	slideShadows: true,
-// 	limitRotation: true,
-// },
-effect:"coverFlow",coverFlowEffect:{rorate:20,stretch:50,slideShadows:!0},centeredSlides:!0,autoHeight:!0});//# sourceMappingURL=index.928ed886.js.map
-
-//# sourceMappingURL=index.928ed886.js.map
