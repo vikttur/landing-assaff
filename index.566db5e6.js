fdcc299836a0393f41d6220cd818fe3533228adc
vikttur <105513387+vikttur@users.noreply.github.com>
2023-11-25 20:49:04 +0000

    Deploying to gh-pages from @ vikttur/landing-assaff@0a4eb024b8a5f9444287fb96e705015a2228ef2a 🚀

--- index.566db5e6.js
+++
@@ -1,3 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector(".backdrop")};function e(){o.modal.classList.toggle("is-hidden")}console.log(o),console.log(o.openModalBtn),console.log(o.closeModalBtn),console.log(o.modal),o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();//# sourceMappingURL=index.566db5e6.js.map
-
-//# sourceMappingURL=index.566db5e6.js.map
---
+++ index.7af02b77.js
@@ -0,0 +1,3 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector(".backdrop")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();//# sourceMappingURL=index.7af02b77.js.map
+
+//# sourceMappingURL=index.7af02b77.js.map
