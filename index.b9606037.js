2626aa67e88f6ed2228f30ef1f72a98726177d51
vikttur <105513387+vikttur@users.noreply.github.com>
2023-11-30 11:52:25 +0000

    Deploying to gh-pages from @ vikttur/landing-assaff@1649abf5a7ef7175ef729b6b715112902892edea 🚀

--- index.b9606037.js
+++
@@ -1,4 +0,0 @@
-var e,t,n,c,o;e=document.querySelector(".js-menu-container"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=document.querySelector(".js-navigation-menu"),o=function(){var n="true"===t.getAttribute("aria-expanded");t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),bodyScrollLock[n?"enableBodyScroll":"disableBodyScroll"](document.body)},t.addEventListener("click",o),n.addEventListener("click",o),e.addEventListener("click",function(e){e.target.classList.contains("menu-container__link")&&o()}),c.addEventListener("click",o),// Close the mobile menu on wider screens if the device orientation changes
-window.matchMedia("(min-width: 768px)").addEventListener("change",function(n){n.matches&&(e.classList.remove("is-open"),t.setAttribute("aria-expanded",!1),bodyScrollLock.enableBodyScroll(document.body))});//# sourceMappingURL=index.b9606037.js.map
-
-//# sourceMappingURL=index.b9606037.js.map
